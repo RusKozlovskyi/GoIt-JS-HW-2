20d43cfa52ea0f1a04fb553e38d3c46d65065eb5
RusKozlovskyi <62113685+RusKozlovskyi@users.noreply.github.com>
2020-07-30 18:49:08 +0300

    fix task 3

--- task-3.js
+++ task-3.js
@@ -3,16 +3,17 @@ const findlongestWord = function (string) {
   let longestWord = words[0];
 
   let len = words.length;
+
   for (let i = 1; i < len; i += 1) {
-    if (longestWord.length < words[1].length) {
-      longestWord = words[1];
+    if (longestWord.length < words[i].length) {
+      longestWord = words[i];
     }
   }
   return longestWord;
 };
 
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+console.log(findlongestWord("The quick brown fox jumped over the lazy dog"));
 
-// console.log(findLongestWord("Google do a roll")); // 'Google'
+console.log(findlongestWord("Google do a roll")); // 'Google'
 
-// console.log(findLongestWord("May the force be with you")); // 'force'
+console.log(findlongestWord("May the force be with you")); // 'force'
